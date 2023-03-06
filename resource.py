62d871d96a379e9cd6653586ac448ce3e607162f
Hemanth <hemanththebest@gmail.com>
2023-03-06 03:52:00 -0500

    master:   Added resource file and requirements.txt

---
+++ resource.py
@@ -0,0 +1,37 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+# from flask_cors import CORS, cross_origin
+from flask import Flask
+
+load_dotenv()
+app = Flask(__name__)
+# cors = CORS(app)
+# app.config['CORS_HEADERS'] = 'Content-Type'
+app.debug = True
+db_url = os.getenv("DATABASE_URL")
+conn = psycopg2.connect(db_url)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.get("/getUsers")
+def getUsers():
+    with conn:
+        with conn.cursor() as cursor:
+            cursor.execute("select distinct userid from ratings order by userid")
+            data = cursor.fetchall()
+    return {"users": [x[0] for x in data]}
+
+@app.route("/getTopRecommendations")
+def getTopRecommendations():
+    return "Top Recommendations"
+
+@app.route("/getTopRecommendationsGenres")
+def getTopRecommendationsByGenres():
+    return "Top Recommendations By Genres"
+
+@app.route("/getSimilarRecommendations")
+def getSimilarRecommendations():
+    return "Similar Recommendations"
